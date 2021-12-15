b8bbfbf8c896795f4218f8fcb010c204dc98ab32
Luisa Muñoz <munozluisa643@gmail.com>
2021-12-15 10:26:37 -0500

    Agora app builder initalized, npm initialized, PollContext, PollStyles.js, Poll.jsx components created, App.tsx wrapped with PollContext, and HostControlView.tsx new Views and Poll question added

---
+++ agora-app-builder/greatingChatApp/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  greatingChatApp
+//
+
+import Foundation
---
+++ agora-app-builder/greatingChatApp/src/components/Poll.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useContext } from 'react';
+import Modal from 'react-modal';
+import { Line } from 'rc-progress';
+
+import { PollContext } from './PollContext';
+
+const Poll = () => {
+    const { question, setQuestions, answers: voteData, setAnswers, isModalOpen, setIsModalOpen } = useContext(PollContext);
+    const [totalVotes, setTotalVotes] = useState(0);
+    const [voted, setVoted] = useState(false);
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setTotalVotes(0);
+        setVoted(false);
+        setQuestions('');
+        setAnswers([
+            {Option: '', votes: 0},
+            {Option: '', votes: 0},
+            {Option: '', votes: 0},
+            {Option: '', votes: 0},
+        ]);
+    }
+
+    return (
+        <Modal
+            isOpen= {isModalOpen}
+            onRequestClose= {closeModal}
+            content= "PollModal"
+            style={style.customStyles}
+        >
+
+        </Modal>
+    )
+}
+
+export default Poll;
